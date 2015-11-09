673bca74092e2d5781c9ebb9a79c4c83bea6b843
李蝉 <10915819@qq.com>
2015-11-09 11:05:10 +0800

    init
    init

---
+++ RedefineTabBarController/RedefineTabBarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedefineTabBarController
+//
+//  Created by Ceres on 15/11/9.
+//  Copyright © 2015年 www.upupapp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedefineTabBarController/RedefineTabBarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedefineTabBarController
+//
+//  Created by Ceres on 15/11/9.
+//  Copyright © 2015年 www.upupapp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
